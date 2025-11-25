a722d65809d6b6a44ee54bb13777a74f6f574b87
DehliFin <dehli1212@gmail.com>
2025-11-25 15:35:21 +0100

    crud

--- ArtMuseumAPI/Controllers/CollectionsController.cs
+++ ArtMuseumAPI/Controllers/CollectionsController.cs
@@ -1,4 +1,5 @@
-﻿using ArtMuseumAPI.Models;
+﻿using ArtMuseumAPI.DTO;
+using ArtMuseumAPI.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -52,6 +53,54 @@ namespace ArtMuseumAPI.Controllers.Mysql
                 .FirstOrDefaultAsync();
 
             return collection is null ? NotFound() : Ok(collection);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCollection([FromBody] AddCollectionRequest request)
+        {
+            if (request is null)
+                return BadRequest("Request body is required.");
+
+            if (request.Name.Length >255)
+                return BadRequest("Name is too long. It must be less than 255 characters long");
+
+
+            var collection = new Collection
+            {
+                Name = request.Name,
+                Description = request.Description,
+                OwnerId = request.Owner
+            };
+
+            _db.Collections.Add(collection);
+
+            await _db.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetCollectionId), new { id = collection.CollectionId }, new
+            {
+                collection.CollectionId,
+                collection.OwnerId,
+                collection.Name,
+                collection.Description
+            });
+        }
+
+        [HttpPut("{id:int}/{ownerId:int}")]
+        public async Task<IActionResult> UpdateOwnerOnCollection(int id,int ownerId)
+        {
+            var collection = await _db.Collections
+                    .FirstOrDefaultAsync(c => c.CollectionId == id);
+
+            if (collection == null)
+            {
+                return NotFound();
+            }
+
+            collection.OwnerId = ownerId;
+
+            await _db.SaveChangesAsync();
+
+            return Ok(collection);
         }
     }
 }
